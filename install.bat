42125671cdfa029ef6b375a8db34a69861b2a3d6
userzaqo <75968937+userzaqo@users.noreply.github.com>
2020-12-14 08:31:37 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,4 @@
+npm i
+PAUSE
+echo "Successfully Installed Package ! now open start.bat or run by node nukes.js"
+
---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+color 4
+title .                                                                                                                                          + zaqo.sh +                                                                                                     .
+:top
+cls
+node nuke.js
+pause
+goto top
